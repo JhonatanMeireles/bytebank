4cb5a82ec5fc426131340b0c0eba03a6d15cc97e
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-11-11 17:09:06 -0300

    Add project files.

---
+++ SupermercadoMenorPreco/Program.cs
@@ -0,0 +1,10 @@
+﻿using SupermercadoMenorPreco;
+
+Produto produto1 = new Produto() ;
+
+produto1.nomeProduto = "arroz";
+produto1.fabricante = "Tio João";
+produto1.codigoDeBarras = "Ar123roz";
+produto1.valorUnitario = 8.03;
+produto1.valorCompra = 6.85;
+produto1.quantidadeEmEstoque = 23;
---
+++ bytebank/Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_Agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine($"O saldo da conta de {contaDoAndre.titular} é de R$ {contaDoAndre.saldo}.");
