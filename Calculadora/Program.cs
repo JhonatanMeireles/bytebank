f24a1d75b785d4213d49228f00e6aad6c8f7879d
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-11-20 23:22:53 -0300

    Algumas ações realizadas no Desafio2

---
+++ Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculadora;
+
+Soma soma = new Soma();
+soma.AdicionarNumeroSoma(1);
+soma.AdicionarNumeroSoma(2);
+soma.AdicionarNumeroSoma(3);
+soma.Somar();
---
+++ Calculadora/Soma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Soma
+    {
+        List<int> numeros = new List<int>();
+
+         
+        public void AdicionarNumeroSoma(int num) 
+        {
+            this.numeros.Add(num);
+        }
+        public void Somar() 
+        {
+            int number = 0;
+            foreach (int i in numeros)
+            {
+                number += i;
+            
+            }
+            Console.WriteLine(number);
+        }
+
+    }
+}
